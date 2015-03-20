cfa28a583136a81e8c6f61b309dddbc91aab6478
Mark anthony novilla <ton360lyfe@yahoo.com>
2015-03-20 10:49:37 -0700

    cleaned up the pins scaffold

--- app/views/pages/profile.html.erb
+++
@@ -1,1 +0,0 @@
-<h1>Welcome to your profile page</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
  root 'pages#home'
-
- get 'about' => 'pages#about'
-
- get 'profile' => 'pages#profile'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
