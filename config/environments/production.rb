7468333973c845999f6e34c3e06a57ceb72bd6a3
Mark anthony novilla <ton360lyfe@yahoo.com>
2015-03-19 23:41:32 -0700

    debuggin the "onfig" shakespere name error running the rake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # required by heroku
-   onfig.action_mailer.default_url_options = { :host => 'http://mariit.herokuapp.com/' }
+   config.action_mailer.default_url_options = { :host => 'http://mariit.herokuapp.com/' }
 end
